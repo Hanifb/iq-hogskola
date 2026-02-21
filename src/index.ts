526423e9a6a64d0958f6c7125d11be65654f9ea9
Hanif Bali <hanif.bali@gmail.com>
2026-02-21 10:35:56 +0100

    Update Open Graph and Twitter meta tags for embedtest; add route for twitter player

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,14 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '..', 'components', 'hogskola.htm'))
+})
+
+app.get('/twitter-player.html', (_req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'twitter-player.html'))
 })
 
 
